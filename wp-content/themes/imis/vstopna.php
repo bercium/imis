596b3bf79fe157c49f5001b19bf8e71c7015c267
Jure <jure.ravlic@gmail.com>
2013-09-16 09:30:33 +0200

    advanced custom field plugin

--- wp-content/themes/imis/vstopna.php
+++ wp-content/themes/imis/vstopna.php
@@ -12,22 +12,43 @@ get_header(); ?>
 
 <div class="row" ><img src="<?php bloginfo('template_directory'); ?>/img/brisime.jpg"></div>
 
- <div class="row content-wrapper">
-      
-              <!-- REPLACE  wp-php here on: -->
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-  <article class="post" id="post-<?php the_ID(); ?>">
-  
-    <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+         <div class="row content-wrapper">
 
-    <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-  
-  </article>
-  <?php endwhile; endif; ?>
-  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+              <div class="home-sidebar one-third">
+                    <div class="text-wrap">
+                       <?php the_field('levi_stolpec'); ?>
+                    </div>
+              </div>
 
-  <?php// comments_template(); ?>
-              <!-- wp-php end  -->
+              <div class="home-main-content one-third">
+                    <div class="text-wrap">
+                           <!-- REPLACE  wp-php here on: -->
+              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+              <article class="post" id="post-<?php the_ID(); ?>">
+              
+                <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+
+                <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+              
+              </article>
+              <?php endwhile; endif; ?>
+              <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+              <?php// comments_template(); ?>
+                          <!-- wp-php end  -->
+                    </div>
+              </div>
+
+              <div class="home-sidebar one-third">
+                  <div class="text-wrap">
+                    <!-- REPLACE wp-php here on: -->
+                    <h2>Kaj delamo?</h2>
+                     <p>Razvoj programskega produkta IMiS® za skeniranje, arhiviranje, pregledovanje, razvrščanje in iskanje dokumentov, ter povezovanje z aplikacijami. Razvoj programskega produkta IMiS® 
+                    za skeniranje, arhiviranje, pregledovanje, razvrščanje in iskanje dokumentov, ter povezovanje z aplikacijami. </p>
+                    <!-- wp-php end  -->
+                    </div>
+              </div>
+
           </div>
       </div>
                  
