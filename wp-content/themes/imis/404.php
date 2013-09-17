69fc6a1de68b55e77cbb0518d41930b5bc6ea716
Jure <jure.ravlic@gmail.com>
2013-09-17 19:37:24 +0200

    footer horizontal align fix

--- wp-content/themes/imis/404.php
+++ wp-content/themes/imis/404.php
@@ -3,18 +3,48 @@
  * @package WordPress
  * @subpackage HTML5_Boilerplate
  */
-
 get_header(); ?>
 
-  <div id="main" role="main">
+<div class="vs vs-default <?php the_field('slika-v-glavi'); ?>" ></div>
 
-    <details>
-      <summary><h1>Not found</h1></summary>
+
+ <div class="row">
+      <div class="sidebar-one sidebar">
+          <div class="text-wrap">
+              <!-- REPLACE  wp-php here on: -->
+              <img src="<?php bloginfo('template_directory'); ?>/img/secondary-logo.png" title="IMIS"  />
+                  <?php  // left sidebar / secondary menu menu ?>
+                  <ul class="clearfix">
+                   
+                  </ul>
+          <!-- wp-php end  -->
+          </div>
+      </div>
+
+
+      <div class="main-content">
+          <div class="text-wrap">
+              <details>
+      <summary><h1>Napaka 404</h1></summary>
       <p><span frown>:(</span></p>
     </details>
+          </div>
+      </div>
+      <div class="sidebar-two sidebar">
+        <div class="img-box" ></div>
+        <div class="text-wrap">
 
+          <!-- REPLACE wp-php here on: -->
+            <?php 
+            // right side sidebar
+            dynamic_sidebar();
+            //get_sidebar(); ?>
+
+          <!-- wp-php end  -->
+          </div>
+      </div>            
   </div>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
