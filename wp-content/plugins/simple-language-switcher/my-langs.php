e2c23c294c6fd8bb7bc6c8f0a1e902e4fe88f200
Ziga Berce <bercium@gmail.com>
2013-09-26 18:24:09 +0200

    New DB fix for english slider

--- wp-content/plugins/simple-language-switcher/my-langs.php
+++ wp-content/plugins/simple-language-switcher/my-langs.php
@@ -1,4 +1,4 @@
 <?php return array(
 	'sl' => array( 'lang-iso' => 'sl', 'lang-name' => 'SLO', 'lang-home-url' => 'http://127.0.0.1/razvoj/othr/imis/', 'lang-regex' => '' ),
-	'en' => array( 'lang-iso' => 'en', 'lang-name' => 'ENG', 'lang-home-url' => 'http://127.0.0.1/razvoj/othr/imis/', 'lang-regex' => '~/en~' ),
+	'en' => array( 'lang-iso' => 'en', 'lang-name' => 'ENG', 'lang-home-url' => 'http://127.0.0.1/razvoj/othr/imis/en/entry/', 'lang-regex' => '~/en~' ),
 ); ?>
