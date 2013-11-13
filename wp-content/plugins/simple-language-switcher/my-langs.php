f127009afb1c91a02bda1c9f681edcd035ab8fcd
Ziga Berce <bercium@gmail.com>
2013-11-13 19:04:45 +0100

    langswitcher

--- wp-content/plugins/simple-language-switcher/my-langs.php
+++ wp-content/plugins/simple-language-switcher/my-langs.php
@@ -1,3 +1,10 @@
+<?php 
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://"; 
+$domain = $_SERVER['HTTP_HOST']; 
+return array( 
+        'sl' => array( 'lang-iso' => 'sl', 'lang-name' => 'SLO', 'lang-home-url' => $protocol.$domain.'/', 'lang-regex' => '' ), 
+        'en' => array( 'lang-iso' => 'en', 'lang-name' => 'ENG', 'lang-home-url' => $protocol.$domain.'/en/entry', 'lang-regex' => '~/en~' ), 
+); ?> 
 <?php return array(
 	'sl' => array( 'lang-iso' => 'sl', 'lang-name' => 'SLO', 'lang-home-url' => 'http://127.0.0.1/razvoj/othr/imis/', 'lang-regex' => '' ),
 	'en' => array( 'lang-iso' => 'en', 'lang-name' => 'ENG', 'lang-home-url' => 'http://127.0.0.1/razvoj/othr/imis/en/entry', 'lang-regex' => '~/en~' ),
